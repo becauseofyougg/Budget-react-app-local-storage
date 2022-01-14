86e060abeb9febf4b30febbc52bb2e7c4ebed2a1
becauseofyougg <87302450+becauseofyougg@users.noreply.github.com>
2022-01-14 16:06:33 +0300

    expenseFix

--- src/components/AddExpenseModal.js
+++ src/components/AddExpenseModal.js
@@ -1,16 +1,23 @@
 import { Form, Modal, Button } from "react-bootstrap"
 import { useRef } from "react"
-import { useBudgets } from "../contexts/BudgetsContext"
+import { useBudgets, UNCATEGORIZED_BUDGET_ID } from "../contexts/BudgetsContext"
 
-export default function AddBudgetModal({ show, handleClose }) {
-  const nameRef = useRef()
-  const maxRef = useRef()
-  const { addBudget } = useBudgets()
+export default function AddExpenseModal({
+  show,
+  handleClose,
+  defaultBudgetId,
+}) {
+  const descriptionRef = useRef()
+  const amountRef = useRef()
+  const budgetIdRef = useRef()
+  const { addExpense, budgets } = useBudgets()
+
   function handleSubmit(e) {
     e.preventDefault()
-    addBudget({
-      name: nameRef.current.value,
-      max: parseFloat(maxRef.current.value),
+    addExpense({
+      description: descriptionRef.current.value,
+      amount: parseFloat(amountRef.current.value),
+      budgetId: budgetIdRef.current.value,
     })
     handleClose()
   }
@@ -19,22 +26,33 @@ export default function AddBudgetModal({ show, handleClose }) {
     <Modal show={show} onHide={handleClose}>
       <Form onSubmit={handleSubmit}>
         <Modal.Header closeButton>
-          <Modal.Title>New Budget</Modal.Title>
+          <Modal.Title>New Expense</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <Form.Group className="mb-3" controlId="name">
-            <Form.Label>Name</Form.Label>
-            <Form.Control ref={nameRef} type="text" required />
+          <Form.Group className="mb-3" controlId="description">
+            <Form.Label>Description</Form.Label>
+            <Form.Control ref={descriptionRef} type="text" required />
           </Form.Group>
-          <Form.Group className="mb-3" controlId="max">
-            <Form.Label>Maximum Spending</Form.Label>
+          <Form.Group className="mb-3" controlId="amount">
+            <Form.Label>Amount</Form.Label>
             <Form.Control
-              ref={maxRef}
+              ref={amountRef}
               type="number"
               required
               min={0}
               step={0.01}
             />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="budgetId">
+            <Form.Label>Budget</Form.Label>
+            <Form.Select defaultValue={defaultBudgetId} ref={budgetIdRef}>
+              <option id={UNCATEGORIZED_BUDGET_ID}>Uncategorized</option>
+              {budgets.map(budget => (
+                <option key={budget.id} value={budget.id}>
+                  {budget.name}
+                </option>
+              ))}
+            </Form.Select>
           </Form.Group>
           <div className="d-flex justify-content-end">
             <Button variant="primary" type="submit">
